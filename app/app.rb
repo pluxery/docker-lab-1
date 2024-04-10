2b22a15cdba9fcb3ada02d048957d08e07fd4e0c
Семен Дранов <dranov.sv@dvfu.ru>
2024-04-10 16:53:15 +1000

    css

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,15 @@ require 'sinatra/reloader'
 set :bind, '0.0.0.0'
 set :port, 3000
 
+set :views, File.join(File.dirname(__FILE__), 'views')
+set :public_folder, File.join(File.dirname(__FILE__), 'public')
+
+
 get '/' do
-  erb :index
+  erb :index, layout: :layout
+end
+
+
+get '/page' do
+  erb :page, layout: :layout
 end
