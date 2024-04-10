47403464ea1f7cb0280abe691352216d753661f0
Семен Дранов <dranov.sv@dvfu.ru>
2024-04-10 16:25:13 +1000

    add erb

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra' 
+gem 'sinatra-contrib'
 gem 'pg' 
 gem 'rspec', group: :test 
 gem 'rackup', '~> 2.1'
--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 set :bind, '0.0.0.0'
 set :port, 3000
 
 get '/' do
-  'Поставьте автомат, пожалуйста :3'
+  erb :index
 end
